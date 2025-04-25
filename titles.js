56a46d7233e6a48366b61fc4fbae6f3bca092c9a
logan <lolomeerkat@icloud.com>
2025-04-25 13:15:41 -0700

    Update titles.js

--- titles.js
+++ titles.js
@@ -4,5 +4,7 @@ const titles = [
 { name: "Drifter", rarity: "Rare", color: "#4444ff", prob: 0.75 },
 { name: "Pulsebound", rarity: "Uncommon", color: "#66ccff", prob: 0.6 },
 { name: "Neon Rookie", rarity: "Common", color: "#cccccc", prob: 0.4 },
+  { name: "skibidi", rarity: "Common", color: "#cccccc", prob: 0.3 },  
+  { name: "ohio", rarity: "Common", color: "#22ff00", prob: 0.1 },
 // Repeat this pattern to fill up 50-100 titles with creative names, colors, and rarities
 ];
